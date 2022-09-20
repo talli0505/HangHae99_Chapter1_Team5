236b9a41bce55afa04ffd20952e011f09f3d5c59
조민수 <87609692+talli0505@users.noreply.github.com>
2022-09-20 19:11:10 +0900

    [09/20] 댓글 추가,삭제 + 댓글버튼,좋아요를 위한 뼈대 준비

--- Chapter1/app_comment.py
+++ Chapter1/app_comment.py
@@ -1,7 +1,7 @@
 # DB를 위한 코드
 from pymongo import MongoClient
 
-client = MongoClient('몽고디비주소')
+client = MongoClient('자기 몽고디비 ')
 db = client.dbsparta
 
 # Flask를 위한 코드
@@ -17,6 +17,7 @@ from bs4 import BeautifulSoup
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/comment/<title>')
 def detail(title):
@@ -34,7 +35,8 @@ def save_comment():
     doc = {
         "num": count,
         "nickname": nickname_receive,
-        "comment": comment_receive
+        "comment": comment_receive,
+        "like": 0
     }
 
     db.musics.insert_one(doc)
@@ -53,6 +55,25 @@ def delete_word():
 
     db.musics.delete_one({'num': int(num_receive)})
     return jsonify({'msg': '삭제 완료!'})
+
+
+# 음원 데이터 크롤링 이후 ajax파일로 테이블을 만든 다음에 이용
+# @app.route("/like", methods=["POST"])
+# def like_heart():
+#     num_receive = request.form['num_give']
+#
+#     db.musics.update_one({'num': int(num_receive)}, {'$set': {'like': 1}})
+#
+#     return jsonify({'msg': '좋아요 완료!'})
+#
+#
+# @app.route("/dislike", methods=["POST"])
+# def dislike_dislike():
+#     num_receive = request.form['num_give']
+#
+#     db.bucket.update_one({'num': int(num_receive)}, {'$set': {'like': 0}})
+#
+#     return jsonify({'msg': '취소 완료!'})
 
 
 if __name__ == '__main__':
