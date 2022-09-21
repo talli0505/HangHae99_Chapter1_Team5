c198a6631a7d2edc2152a63506bb312ec1b01cba
조민수 <87609692+talli0505@users.noreply.github.com>
2022-09-21 21:51:15 +0900

    [9/21] 회원가입, 로그인, 좋아요, 댓글창 완료

--- Chapter1/static/protocol.js
+++ Chapter1/static/protocol.js
@@ -1,6 +1,4 @@
 // 음악 전체
-
-
 function show_music_all() {
     $('#music_table').empty()
     $.ajax({
@@ -21,7 +19,7 @@ function show_music_all() {
                             <td>${rank}</td>
                             <td>${title}</td>
                             <td>${artist}</td>
-                            <td><button onclick="warning()" id="comment-button" type="button" class="btn btn-primary" data-bs-target="#myModal">댓글버튼</button>👈</td>
+                            <td><button onclick="warning()" id="comment-button" type="button" class="btn btn-primary">댓글버튼</button>👈</td>
                             <td><i onclick="warning()" class="fa-regular fa-heart"></i></td>
                         </tr>`
                 } else {
@@ -29,8 +27,8 @@ function show_music_all() {
                             <td>${rank}</td>
                             <td>${title}</td>
                             <td>${artist}</td>
-                            <td><button onclick="warning()" id="comment-button" type="button" class="btn btn-primary" data-bs-target="#myModal">댓글버튼</button>👈</td>
-                            <td><i onclick="warning()" class="fa-solid fa-heart"></i></td>
+                            <td><button onclick="warning()" id="comment-button" type="button" class="btn btn-primary">댓글버튼</button>👈</td>
+                            <td><i onclick="warning()" class="fa-regular fa-heart"></i></td>
                         </tr>`
                 }
                 $('#music_table').append(temp_html)
@@ -56,7 +54,7 @@ function show_music_all() {
                             <td>${rank}</td>
                             <td>${title}</td>
                             <td>${artist}</td>
-                            <td><button onclick="warning()" type="button" class="btn btn-primary"  data-bs-target="#myModal">댓글버튼</button>👈</td>
+                            <td><button onclick="warning()" id="comment-button" type="button" class="btn btn-primary">댓글버튼</button>👈</td>
                             <td><i onclick="warning()" class="fa-regular fa-heart"></i></td>
                         </tr>`
                 } else {
@@ -64,8 +62,8 @@ function show_music_all() {
                             <td>${rank}</td>
                             <td>${title}</td>
                             <td>${artist}</td>
-                            <td><button onclick="warning()" id="comment-button" type="button" class="btn btn-primary" data-bs-target="#myModal">댓글버튼</button>👈</td>
-                            <td><i onclick="warning()" class="fa-solid fa-heart"></i></td>
+                            <td><button onclick="warning()" id="comment-button" type="button" class="btn btn-primary">댓글버튼</button>👈</td>
+                            <td><i onclick="warning()" class="fa-regular fa-heart"></i></td>
                         </tr>`
                 }
                 $('#music_table').append(temp_html)
@@ -90,16 +88,16 @@ function show_music_all() {
                             <td>${rank}</td>
                             <td>${title}</td>
                             <td>${artist}</td>
-                            <td><button id="comment-button" type="button" class="btn btn-primary"  data-bs-target="#myModal">댓글버튼</button>👈</td>
-                            <td><i onclick="genie_2020_like(${num})" class="fa-regular fa-heart"></i></td>
+                            <td><button onclick="warning()" id="comment-button" type="button" class="btn btn-primary">댓글버튼</button>👈</td>
+                            <td><i onclick="warning()" class="fa-regular fa-heart"></i></td>
                         </tr>`
                 } else {
                     temp_html = `<tr>
                             <td>${rank}</td>
                             <td>${title}</td>
                             <td>${artist}</td>
-                            <td><button id="comment-button" type="button" class="btn btn-primary"  data-bs-target="#myModal">댓글버튼</button>👈</td>
-                            <td><i onclick="genie_2020_dislike(${num})" class="fa-solid fa-heart"></i></td>
+                            <td><button onclick="warning()" id="comment-button" type="button" class="btn btn-primary">댓글버튼</button>👈</td>
+                            <td><i onclick="warning()" class="fa-regular fa-heart"></i></td>
                         </tr>`
                 }
                 $('#music_table').append(temp_html)
@@ -133,16 +131,16 @@ function show_music_2000() {
                             <td>${rank}</td>
                             <td>${title}</td>
                             <td>${artist}</td>
-                            <td><button id="comment-button" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">댓글버튼</button>👈</td>
-                            <td><i onclick="genie_2000_like(${num})" class="fa-regular fa-heart"></i></td>
+                            <td><button onclick="warning()" id="comment-button" type="button" class="btn btn-primary">댓글버튼</button>👈</td>
+                            <td><i onclick="warning()" class="fa-regular fa-heart"></i></td>
                         </tr>`
                 } else {
                     temp_html = `<tr>
                             <td>${rank}</td>
                             <td>${title}</td>
                             <td>${artist}</td>
-                            <td><button id="comment-button" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">댓글버튼</button>👈</td>
-                            <td><i onclick="genie_2000_dislike(${num})" class="fa-solid fa-heart"></i></td>
+                            <td><button onclick="warning()" id="comment-button" type="button" class="btn btn-primary">댓글버튼</button>👈</td>
+                            <td><i onclick="warning()" class="fa-regular fa-heart"></i></td>
                         </tr>`
                 }
                 $('#music_table').append(temp_html)
@@ -172,16 +170,16 @@ function show_music_2010() {
                             <td>${rank}</td>
                             <td>${title}</td>
                             <td>${artist}</td>
-                            <td><button id="comment-button" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">댓글버튼</button>👈</td>
-                            <td><i onclick="genie_2010_like(${num})" class="fa-regular fa-heart"></i></td>
+                            <td><button onclick="warning()" id="comment-button" type="button" class="btn btn-primary">댓글버튼</button>👈</td>
+                            <td><i onclick="warning()" class="fa-regular fa-heart"></i></td>
                         </tr>`
                 } else {
                     temp_html = `<tr>
                             <td>${rank}</td>
                             <td>${title}</td>
                             <td>${artist}</td>
-                            <td><button id="comment-button" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">댓글버튼</button>👈</td>
-                            <td><i onclick="genie_2010_dislike(${num})" class="fa-solid fa-heart"></i></td>
+                            <td><button onclick="warning()" id="comment-button" type="button" class="btn btn-primary">댓글버튼</button>👈</td>
+                            <td><i onclick="warning()" class="fa-regular fa-heart"></i></td>
                         </tr>`
                 }
                 $('#music_table').append(temp_html)
@@ -211,99 +209,20 @@ function show_music_2020() {
                             <td>${rank}</td>
                             <td>${title}</td>
                             <td>${artist}</td>
-                            <td><button id="comment-button" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">댓글버튼</button>👈</td>
-                            <td><i onclick="genie_2020_like(${num})" class="fa-regular fa-heart"></i></td>
+                            <td><button onclick="warning()" id="comment-button" type="button" class="btn btn-primary">댓글버튼</button>👈</td>
+                            <td><i onclick="warning()" class="fa-regular fa-heart"></i></td>
                         </tr>`
                 } else {
                     temp_html = `<tr>
                             <td>${rank}</td>
                             <td>${title}</td>
                             <td>${artist}</td>
-                            <td><button id="comment-button" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">댓글버튼</button>👈</td>
-                            <td><i onclick="genie_2020_dislike(${num})" class="fa-solid fa-heart"></i></td>
+                            <td><button onclick="warning()" id="comment-button" type="button" class="btn btn-primary">댓글버튼</button>👈</td>
+                            <td><i onclick="warning()" class="fa-regular fa-heart"></i></td>
                         </tr>`
                 }
                 $('#music_table').append(temp_html)
             }
-        }
-    });
-}
-
-function genie_1990_like(num) {
-    $.ajax({
-        type: "POST",
-        url: "/genie_1990/like",
-        data: {num_give: num},
-        success: function (response) {
-            window.location.reload()
-        }
-    });
-}
-
-
-function genie_2000_like(num) {
-    $.ajax({
-        type: "POST",
-        url: "/genie_2000/like",
-        data: {num_give: num},
-        success: function (response) {
-            window.location.reload()
-        }
-    });
-}
-
-function genie_2010_like(num) {
-    $.ajax({
-        type: "POST",
-        url: "/genie_2010/like",
-        data: {num_give: num},
-        success: function (response) {
-            window.location.reload()
-        }
-    });
-}
-
-function genie_2020_like(num) {
-    $.ajax({
-        type: "POST",
-        url: "/genie_2020/like",
-        data: {num_give: num},
-        success: function (response) {
-            window.location.reload()
-        }
-    });
-}
-
-
-function genie_2000_dislike(num) {
-    $.ajax({
-        type: "POST",
-        url: "/genie_2000/dislike",
-        data: {num_give: num},
-        success: function (response) {
-            window.location.reload()
-        }
-    });
-}
-
-function genie_2010_dislike(num) {
-    $.ajax({
-        type: "POST",
-        url: "/genie_2010/dislike",
-        data: {num_give: num},
-        success: function (response) {
-            window.location.reload()
-        }
-    });
-}
-
-function genie_2020_dislike(num) {
-    $.ajax({
-        type: "POST",
-        url: "/genie_2020/dislike",
-        data: {num_give: num},
-        success: function (response) {
-            window.location.reload()
         }
     });
 }
@@ -596,4 +515,52 @@ function sign_in() {
     });
 }
 
+<!-- 포스트 입력 -->
+
+        function post() {
+            let post = $('#input-post').val()
+            $.ajax({
+                type: 'POST',
+                url: '/posting',
+                data: {comment_give: post},
+                success: function (response) {
+                    alert(response['msg'])
+                    window.location.reload()
+                }
+            });
+        }
+
+        function show_post() {
+            console.log('넌 타ㅣ니?');
+            $.ajax({
+                type: 'GET',
+                url: '/comment',
+                data: {},
+                success: function (response) {
+                    console.log(response);
+
+                    let length = response['comments'].length;
+
+                    let rows = response['comments'];
+                    console.log(rows);
+
+                    for (var i = 0; i < length; i++) {
+                        let temp_html = `<div class="box" id="comment-card">
+                                            <article class="media">
+                                                <div class="media-content">
+                                                    <div class="content">
+                                                        <p>
+                                                            <strong>홍길동</strong> <small>@username</small> <small>10분 전</small>
+                                                            <br>
+                                                            ${rows[i]['comment']}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </article>`;
+                        $('#post-box').append(temp_html)
+                    }
+
+                }
+            });
+        }
 
